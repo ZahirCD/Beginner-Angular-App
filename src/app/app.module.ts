3444ccb3cc9972a7c4f5038bdfc2202473f3c31c
Zahir <zahir.khan1233@gmail.com>
2023-12-18 13:44:21 -0500

    Completed Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import{ SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+  
   ],
   imports: [
     BrowserModule,
