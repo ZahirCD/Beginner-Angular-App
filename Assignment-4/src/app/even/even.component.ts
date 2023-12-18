8f608d8f2d60aef665e2052d82131921aaf91c9f
Zahir <zahir.khan1233@gmail.com>
2023-12-18 16:32:48 -0500

    added assignment 4 files

---
+++ Assignment-4/src/app/even/even.component.ts
@@ -0,0 +1,21 @@
+import { Component , OnInit , Input} from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  templateUrl: './even.component.html',
+  styleUrl: './even.component.css'
+})
+export class EvenComponent implements OnInit{
+  @Input() number: number;
+
+
+  constructor(){}
+
+  ngOnInit() {
+    
+  }
+
+
+
+
+}
